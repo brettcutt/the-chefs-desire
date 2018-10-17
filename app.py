b1a1cf7433c8d6209ff1e7d0f82d4d52c8d63fec
Brett Cutting <brettcutt@outlook.com.au>
2018-10-17 02:36:12 +0000

    added variables for cuisine and allergen categories

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os 
+import json
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -9,6 +10,17 @@ app.config["MONGO_DBNAME"] = "meal-ponderer"
 app.config["MONGO_URI"] = "mongodb://admin:Mealponderer1@ds131763.mlab.com:31763/meal-ponderer"
 
 mongo = PyMongo(app)
+
+cuisines_json = []
+
+with open ("data/cuisine_category.json", "r") as file:
+    cuisines_json = json.load(file)
+
+allergens_json = []    
+    
+with open ("data/allergen_category.json", "r") as file:
+    allergens_json = json.load(file)
+    
 
 @app.route("/")
 def index():
@@ -23,17 +35,16 @@ def recipes():
     
 @app.route("/add_recipe")
 def add_recipe():
-    return render_template("add_recipe.html")
+    return render_template("add_recipe.html", cuisines=mongo.db.cuisines.find(), allergens=mongo.db.allergens.find())
 
 @app.route("/insert_recipe", methods=['POST'])
 def insert_recipe():
     doc = {
         "name":request.form.get('name'),
-        "cuisine":request.form.get('cuisine'),
+        "cuisine":(request.form.getlist('cuisine')),
         "allergens":(request.form.getlist('allergens')),
         "description":request.form.get('description'),
-        "ingredients":(request.form.getlist('ingredients')
-        ),
+        "ingredients":(request.form.getlist('ingredients')),
         "instructions":(request.form.getlist('instructions')),
         "prep_time":request.form.get('prep_time'),
         "cook_time":request.form.get('cook_time'),
@@ -48,29 +59,27 @@ def insert_recipe():
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     the_recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
-    return render_template('edit_recipe.html', recipe=the_recipe)
+    return render_template('edit_recipe.html', recipe=the_recipe, other_cuisines=cuisines_json, allergens_json=allergens_json)
     
 @app.route("/update_recipe/<recipe_id>", methods=['POST'])
 def update_recipe(recipe_id):
     
-    if request.form('ingredients') != "":
-        ingredients = request.form.getlist('ingredients')
-        mongo.db.recipe.update({'_id':ObjectId(recipe_id)},
-        {
-            "name":request.form.get('name'),
-            "cuisine":request.form.get('cuisine'),
-            "allergens":request.form.getlist('allergens'),
-            "description":request.form.get('description'),
-            "ingredients":ingredients,
-            "instructions":request.form.getlist('instructions'),
-            "prep_time":request.form.get('prep_time'),
-            "cook_time":request.form.get('cook_time'),
-            "recipe_yield":request.form.get('recipe_yield'),
-            "author":request.form.get('author'),
-            "image":request.form.get('image')
-        })
-        
-        return redirect(url_for('recipes'))
+    mongo.db.recipe.update({'_id':ObjectId(recipe_id)},
+    {
+        "name":request.form.get('name'),
+        "cuisine":request.form.getlist('cuisine'),
+        "allergens":request.form.getlist('allergens'),
+        "description":request.form.get('description'),
+        "ingredients":request.form.getlist('ingredients'),
+        "instructions":request.form.getlist('instructions'),
+        "prep_time":request.form.get('prep_time'),
+        "cook_time":request.form.get('cook_time'),
+        "recipe_yield":request.form.get('recipe_yield'),
+        "author":request.form.get('author'),
+        "image":request.form.get('image')
+    })
+
+    return redirect(url_for('recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"), port=int(os.environ.get('PORT')), debug=True)
