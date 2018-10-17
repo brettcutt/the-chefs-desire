ec7d8e21e56e9b2b9de59784b8ee293f09c6c75e
Brett Cutting <brettcutt@outlook.com.au>
2018-10-17 02:35:03 +0000

    added additional add and remove buttons for add and edit recipe html. cuisine and allergen fields

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -1,13 +1,24 @@
 $(document).ready( function() {
     
-    
-    
+/*Add input field for instruction and ingredients, also adds the add and remove buttons*/    
+   
     $("body").on('click','.duplicate', function(){
+        
         var id_name = $(this).parent().children('input').attr("name");
         
-        $(this).parent().children().last().append().after(
-            '<br><input type="text" name="' + id_name +'" class="allergens" placeholder="Add"/> <i class="fas fa-plus-circle duplicate pointer">Add</i>' +
+        if ($(this).attr('id') == 'first') {
+            $(this).append().after(
+            '<br><input type="text" name="' + id_name +'" class="' + id_name +'" placeholder="Add"/> <i class="fas fa-plus-circle duplicate pointer">Add</i>' +
             ' <i class="fas fa-plus-circle remove_duplicate pointer">Remove</i>');
+        } 
+        
+        else 
+        {
+            $(this).next().append().after(
+            '<br><input type="text" name="' + id_name +'" class="' + id_name +'" placeholder="Add"/> <i class="fas fa-plus-circle duplicate pointer">Add</i>' +
+            ' <i class="fas fa-plus-circle remove_duplicate pointer">Remove</i>');
+        }
+        
     });
     
     $("body").on('click','.remove_duplicate', function(){
@@ -16,5 +27,27 @@ $(document).ready( function() {
         $(this).prev().remove();
         $(this).remove();
     });
+    
+    
+/*Select cuisine and allergens, also addds the remove*/
+    
+    $("body").on('click','.duplicate_select', function(){
+        
+    var select_remove_button = ' <i class="fas fa-plus-circle remove_select pointer">Remove</i><br>';
+    
+        if ($(this).attr('id') == 'add_cuisine') {
+            $("#cuisine").clone().appendTo(".cuisine_container").after(select_remove_button)
+        }
+        else if ($(this).attr('id') == 'add_allergen') {
+            $("#allergens").clone().appendTo(".allergens_container").after(select_remove_button)
+        }
+    });
+    
+    $("body").on('click','.remove_select', function(){
+        $(this).prev().remove();
+        $(this).prev().remove();
+        $(this).remove();
+    });
+    
     
 });
