110ca4a38f1740a4b994fc9743e093aa78d8574b
Brett Cutting <brettcutt@outlook.com.au>
2018-10-23 11:53:14 +0000

    cleaned up code, added function for user in session and iterating over found objects

--- app.py
+++ app.py
@@ -54,12 +54,27 @@ def registration_form():
     }
     return data
     
+# FUNCTION 1
+def find_value(variable):
+    item = ""
+    for key, value in variable.items():
+        if key !="_id":
+            item = value
+    return item
+    
+# FUNCTION 2    
+def if_user_in_session():
+    username = ""
+    if 'user' in session:
+        username = session['user']
+    return username
+    
 # //////////////// INDEX (render)
 
 @app.route("/")
 def index():
-    
-    return render_template('index.html')
+    recipes = mongo.db.recipe.aggregate([{"$sample": {"size": 5}}])
+    return render_template('index.html', recipes=recipes)
     
 # //////////////// SIGN IN
 @app.route('/signin', methods=['POST'])
@@ -74,24 +89,23 @@ def signin():
     try:
         user_doc_username = mongo.db.user_details.find_one({'username':username}, {'username'})
         user_doc_password = mongo.db.user_details.find_one({'username':username}, {'password'})
-        for key, value in user_doc_password.items():
         
-            if key != "_id":
-                stored_password = value
-                if password == stored_password:
-                    session['user'] = username
-        
-                    print("user found")
-                    return redirect(url_for('my_recipes', username=username))
-                    
-            else:
-                session['flash-message1'] = 1
-                test = session['flash-message1']
-                print(test)
-                flash("Incorrect username or password")
-                return redirect(url_for('index'))
+        stored_username = find_value(user_doc_username)# FUNCTION 1
+        stored_password = find_value(user_doc_password)# FUNCTION 1
+        if password == stored_password and username == stored_username:
+            if 'flash-message1' in session:
+                session.pop('flash-message1')
+            session['user'] = username
+            return redirect(url_for('my_recipes', username=username))
+                
+        else:
+            session['flash-message1'] = 1
+            flash("Incorrect username or password")
+            return redirect(url_for('index'))
+            
     except:
-        print("not found")
+        session['flash-message1'] = 1
+        flash("Incorrect username or password")
         return redirect(url_for('index'))
     
 # //////////////// LOGOUT
@@ -116,26 +130,26 @@ def register():
     if comfirm_password == new_password:
         try:
             existing_user = mongo.db.user_details.find_one({'username':requested_username}, {"username"})
-            print(existing_user)
-            if existing_user == None:
-                mongo.db.user_details.insert_one(registration_form())
-                username = requested_username
-                session['user'] = username
             
-                return redirect(url_for('my_recipes', username=username))
+            if existing_user == None:
+                
+                if 'flash-message2' in session:
+                    session.pop('flash-message2')
+                mongo.db.user_details.insert_one(registration_form())
+                session['user'] = requested_username
+                return redirect(url_for('my_recipes', username=requested_username))
+                
             else:
                 session['flash-message2'] = 1
                 flash("Username already exists")
-                print("user name already exists")
                 return redirect(request.referrer)
             
         except:
-            
             return render_template('index.html')
+            
     else:
         session['flash-message2'] = 2
         flash("Passwords are not the same")
-        print("passwords did not match")
         return render_template('index.html',)
         
 
@@ -144,10 +158,10 @@ def register():
 # //////////////// MY RECIPES
 @app.route('/my_recipes/<username>')
 def my_recipes(username):
+    
     user = mongo.db.user_details.find_one({"username":username})
     print(session['user'])
     user_recipes = mongo.db.recipe.find({"username":session['user']})
-    
     
     return render_template('my_recipes.html', user=user, user_recipes=user_recipes, cuisines_json=cuisines_json, allergens_json=allergens_json)
 
@@ -165,28 +179,31 @@ def recipes():
 # UPDATE THE RECIPE VIEWS
 @app.route('/update_view_count/<recipe_id>')
 def update_view_count(recipe_id):
+    
     recipe_views = mongo.db.recipe.find_one({'_id':ObjectId(recipe_id)}, {"views"})
+    user_check = mongo.db.recipe.find_one({'_id':ObjectId(recipe_id)}, {"username"})
     
-    count = []       
-    
-    for key, value in recipe_views.items():
-        if key !="_id":
-            count = value
-    if not count:
-        mongo.db.recipe.update_one({'_id':ObjectId(recipe_id)},{"$set":{"views": 1 }}, upsert = True)
-    elif count >= 0:
-        mongo.db.recipe.update({'_id':ObjectId(recipe_id)},{"$set": {"views": count + 1 }})
-    
-    
-    return redirect(url_for('single_recipe', recipe_id=recipe_id ))
+    count = find_value(recipe_views) # FUNCTION 1  
+    recipe_username = find_value(user_check) # FUNCTION 1
+       
+    if if_user_in_session() != recipe_username: # FUNCTION 2
+        if not count:
+            mongo.db.recipe.update_one({'_id':ObjectId(recipe_id)},{"$set":{"views": 1 }}, upsert = True)
+            
+        elif count >= 0:
+            mongo.db.recipe.update({'_id':ObjectId(recipe_id)},{"$set": {"views": count + 1 }})
+            
+        return redirect(url_for('single_recipe', recipe_id=recipe_id ))
+        
+    else:
+        return redirect(url_for('single_recipe', recipe_id=recipe_id ))
     
 # SINGLE RECIPE
 @app.route('/single_recipe/<recipe_id>')
 def single_recipe(recipe_id):
     the_recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
-    creator_username = ""
-    if 'user' in session:
-        creator_username = session['user']
+    creator_username = if_user_in_session() # FUNCTION 2
+    
     return render_template("single_recipe.html", recipe=the_recipe, cuisines_json=cuisines_json, allergens_json=allergens_json, creator_username=creator_username)
     
     
@@ -195,18 +212,21 @@ def single_recipe(recipe_id):
 def update_like(recipe_id):
     
     recipe_likes = mongo.db.recipe.find_one({'_id':ObjectId(recipe_id)}, {"likes"})
+    user_check = mongo.db.recipe.find_one({'_id':ObjectId(recipe_id)}, {"username"})
     
-    count = []       
-    
-    for key, value in recipe_likes.items():
-        if key !="_id":
-            count = value
-    if not count:
-        mongo.db.recipe.update_one({'_id':ObjectId(recipe_id)},{"$set":{"likes": 1 }}, upsert = True)
-    elif count >= 0:
-        mongo.db.recipe.update({'_id':ObjectId(recipe_id)},{"$set": {"likes": count + 1 }})
+    recipe_username = find_value(user_check)# FUNCTION 1
+    count = find_value(recipe_likes)  # FUNCTION 1
+            
+    if if_user_in_session() != recipe_username: # FUNCTION 2    
+        if not count:
+            mongo.db.recipe.update_one({'_id':ObjectId(recipe_id)},{"$set":{"likes": 1 }}, upsert = True)
+        elif count >= 0:
+            mongo.db.recipe.update({'_id':ObjectId(recipe_id)},{"$set": {"likes": count + 1 }})
+            
+        return redirect(url_for('single_recipe', recipe_id=recipe_id ))
         
-    return redirect(url_for('single_recipe', recipe_id=recipe_id ))
+    else:
+        return redirect(url_for('single_recipe', recipe_id=recipe_id ))
 
 # //////////////// SEARCHING RESULT (render)
 
@@ -278,8 +298,18 @@ def add_recipe():
 def insert_recipe():
     doc = recipe_database()
     
+    username = if_user_in_session() # FUNCTION 2
+        
     mongo.db.recipe.insert_one(doc)
-    return redirect(url_for("recipes"))
+    id_num = mongo.db.recipe.find_one({'name':request.form.get('name'), 'username': username})
+    
+    recipe_id = ""
+    for key, value in id_num.items():
+        if key == "_id":
+            recipe_id = ObjectId(value)
+    
+    
+    return redirect(url_for('single_recipe', recipe_id=recipe_id ))
 
 # //////////////// EDIT RECIPE AND UPDATE RECIPE
 # Edit (render)
@@ -293,7 +323,19 @@ def edit_recipe(recipe_id):
 @app.route("/update_recipe/<recipe_id>", methods=['POST'])
 def update_recipe(recipe_id):
     
+    username = if_user_in_session() # FUNCTION 2
+    
     mongo.db.recipe.update_many({'_id':ObjectId(recipe_id)},{"$set": recipe_database()})
+    
+    id_num = mongo.db.recipe.find_one({'name':request.form.get('name'), 'username': username})
+    
+    recipe_id = ""
+    for key, value in id_num.items():
+        if key == "_id":
+            recipe_id = ObjectId(value)
+    
+    
+    return redirect(url_for('single_recipe', recipe_id=recipe_id ))
 
     return redirect(url_for('recipes'))
     
